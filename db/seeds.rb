65e69534cb967a797110827fe97121f3063425f1
Yass All <yassine.allaouna@gmail.com>
2022-03-22 12:56:02 +0100

    ok index et home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Moto.destroy_all
+
+puts "Creating motos..."
+kawazaki = { model: "GPX 750", brand: "Kawazaki", address: "7 Boundary St, London E2 7JE", power: 150, kilometers: 4000 }
+triumph = { model: "Tiger 800", brand: "Triumph", address: "56A Shoreditch High St, London E1 6PQ", power: 150, kilometers: 3000 }
+
+[kawazaki, triumph].each do |attributes|
+  moto = Moto.create!(attributes)
+  puts "Created #{moto.model}"
+end
+puts "Finished!"
